36584bd5884ae4f64992465d3cec0a7dc57905c2
Sanskar Doijode <sanskardoijode876@gmail.com>
2025-06-01 17:27:29 +0530

    main changes

---
+++ green_mart/src/Component/Navigate.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navigate = () => {
+  return (
+    <>
+    <nav className='bg-emerald-600 flex py-5 px-10 items-center justify-between'>
+    <h2 className='text-2xl'>SanS</h2>
+    <div className='flex gap-10'>
+      <h4 className='text-xl'>About</h4>
+      <h4 className='text-xl'>Contact</h4>
+      <h4 className='text-xl'>Service</h4>
+      <h4 className='text-xl'>Your Account</h4>
+    </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navigate
