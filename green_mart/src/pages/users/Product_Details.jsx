3ece2cc40d3add5eb6607b108c67164a4bd45fc5
Rohan Ramchandra Sawant <142981096+Rohansawant734@users.noreply.github.com>
2025-07-24 23:35:06 +0530

    Temporary save 2

--- green_mart/src/pages/users/Product_Details.jsx
+++ green_mart/src/pages/users/Product_Details.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FiMinus , FiPlus} from "react-icons/fi";
 
 const Product_Details = () => {
+  const stock = 0;
+
+  const [count, setCount] = useState(1)
+
+  const onIncrement = ()=>{
+    setCount(count + 1)
+  }
+
+  const onDecrement = ()=>{
+    setCount(count - 1)
+  }
+
   return (
     <div>
-      <h3>product details</h3>
+      <div>
+        <a href="./src/assets/yippee_image.png" target="_self ">
+          <img src="./src/assets/yippee_image.png" alt="Yippee" />
+        </a>
+        <div>
+          <h1>Rs. 15</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem veritatis, illo impedit consequatur explicabo error cumque sapiente odit aspernatur ex ullam qui, neque maiores perspiciatis esse vitae autem, maxime vero.</p>
+          <p>Availability: {stock} in stock</p>
+          <div>
+            <button onClick={onDecrement}><FiMinus /></button>
+            <span style={{ marginLeft: 10, marginRight: 10 }}>{count}</span>
+            <button onClick={onIncrement}><FiPlus /></button>
+          </div>
+          <button>Add To Cart</button>
+          <button>Buy Now</button>
+        </div>
+      </div>
     </div>
   )
 }
