f0bd683a604861945b0b08674daaf7b5b2544b29
Snehit hajare <snehithajare007@gmail.com>
2025-07-24 14:26:35 +0530

    Cart Components added..

--- green_mart/src/pages/users/Cart.jsx
+++ green_mart/src/pages/users/Cart.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Card from '../../Component/Card'
 const Cart = () => {
   return (
-    <div>
-      <h3>Card</h3>
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-green-200 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto">
+        <Card />
+      </div>
     </div>
   )
 }
