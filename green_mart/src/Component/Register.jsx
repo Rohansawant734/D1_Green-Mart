7f8977378115ba071cb49be77b348ab182fb281e
Rohan Ramchandra Sawant <142981096+Rohansawant734@users.noreply.github.com>
2025-08-09 23:59:19 +0530

    fixed layout for no products in search and wishlist

--- green_mart/src/Component/Register.jsx
+++ green_mart/src/Component/Register.jsx
@@ -17,7 +17,9 @@ function Register() {
   const navigate = useNavigate()
   const { register } = useAuth()
 
-  const onRegister = async () => {
+  const onRegister = async (e) => {
+    e.preventDefault() // prevent page reload on form submit
+
     if (!firstName || !lastName || !email || !phone || !password || !confirmPassword) {
       toast.warn('Please fill all required fields.')
       return
@@ -52,68 +54,107 @@ function Register() {
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-white to-green-400 p-6">
-      <div className="w-full max-w-xl bg-white border border-gray-300 shadow-2xl rounded-lg p-10 min-h-[460px]" >
+      <div className="w-full max-w-xl bg-white border border-gray-300 shadow-2xl rounded-lg p-10 min-h-[460px]">
         <h1 className="text-3xl font-extrabold mb-8 text-center text-gray-900">Register</h1>
 
-        <div className="mb-4">
-          <label className="block mb-1 font-medium text-gray-700">
-            First Name<span className="text-red-500">*</span>
-          </label>
-          <input onChange={(e) => setInfo({ ...info, firstName: e.target.value })} type="text" className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400" required/>
-        </div>
-
-        <div className="mb-4">
-          <label className="block mb-1 font-medium text-gray-700">
-            Last Name<span className="text-red-500">*</span>
-          </label>
-          <input onChange={(e) => setInfo({ ...info, lastName: e.target.value })} type="text" className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400" required/>
-        </div>
-
-        <div className="mb-4">
-          <label className="block mb-1 font-medium text-gray-700">
-            Email<span className="text-red-500">*</span>
-          </label>
-          <input onChange={(e) => setInfo({ ...info, email: e.target.value })} type="email" className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400" required/>
-        </div>
-
-        <div className="mb-4">
-          <label className="block mb-1 font-medium text-gray-700">
-            Phone Number<span className="text-red-500">*</span>
-          </label>
-          <div className="flex items-center">
-            <span className="px-4 py-3 bg-gray-200 border border-gray-300 rounded-l-md text-gray-700">
-              +91-
-            </span>
-            <input value={info.phone} onChange={(e) => setInfo({ ...info, phone: e.target.value })}
-              type="tel" className="w-full px-4 py-3 border border-gray-300 rounded-r-md focus:outline-none focus:ring-4 focus:ring-green-400" required/>
+        <form onSubmit={onRegister}>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-gray-700">
+              First Name<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setInfo({ ...info, firstName: e.target.value })}
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400"
+              required
+            />
           </div>
-        </div>
 
-        <div className="mb-4">
-          <label className="block mb-1 font-medium text-gray-700">
-            Password<span className="text-red-500">*</span>
-          </label>
-          <input onChange={(e) => setInfo({ ...info, password: e.target.value })} type="password" className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400" required/>
-        </div>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-gray-700">
+              Last Name<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setInfo({ ...info, lastName: e.target.value })}
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400"
+              required
+            />
+          </div>
 
-        <div className="mb-6">
-          <label className="block mb-1 font-medium text-gray-700">
-            Confirm Password<span className="text-red-500">*</span>
-          </label>
-          <input onChange={(e) => setInfo({ ...info, confirmPassword: e.target.value })} type="password" className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400" required/>
-        </div>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-gray-700">
+              Email<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setInfo({ ...info, email: e.target.value })}
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400"
+              required
+            />
+          </div>
 
-        <div className="mb-6 text-sm text-center">
-          Already have an account?{' '}
-          <Link to="/login" className="text-green-700 font-semibold hover:underline">
-            Login here
-          </Link>
-        </div>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-gray-700">
+              Phone Number<span className="text-red-500">*</span>
+            </label>
+            <div className="flex items-center">
+              <span className="px-4 py-3 bg-gray-200 border border-gray-300 rounded-l-md text-gray-700">
+                +91-
+              </span>
+              <input
+                type="tel"
+                value={phone}
+                onChange={(e) => setInfo({ ...info, phone: e.target.value })}
+                className="w-full px-4 py-3 border border-gray-300 rounded-r-md focus:outline-none focus:ring-4 focus:ring-green-400"
+                required
+              />
+            </div>
+          </div>
 
-        <button
-          className="w-full py-3 bg-green-600 hover:bg-green-800 text-white text-lg font-semibold rounded-lg transition duration-300" type="submit" onClick={onRegister}>
-          Register
-        </button>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-gray-700">
+              Password<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setInfo({ ...info, password: e.target.value })}
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400"
+              required
+            />
+          </div>
+
+          <div className="mb-6">
+            <label className="block mb-1 font-medium text-gray-700">
+              Confirm Password<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setInfo({ ...info, confirmPassword: e.target.value })}
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-4 focus:ring-green-400"
+              required
+            />
+          </div>
+
+          <div className="mb-6 text-sm text-center">
+            Already have an account?{' '}
+            <Link to="/login" className="text-green-700 font-semibold hover:underline">
+              Login here
+            </Link>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-3 bg-green-600 hover:bg-green-800 text-white text-lg font-semibold rounded-lg transition duration-300"
+          >
+            Register
+          </button>
+        </form>
       </div>
     </div>
   )
--- green_mart/src/pages/users/Wish_list.jsx
+++ green_mart/src/pages/users/Wish_list.jsx
@@ -1,59 +1,60 @@
-import React, { useEffect, useRef } from 'react';
-import { useWishlist } from '../../context/WishlistContext';
-import ProductCard from '../../Component/ProductCard';
-import { useAuth } from '../../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+    import React, { useEffect, useRef } from 'react';
+    import { useWishlist } from '../../context/WishlistContext';
+    import ProductCard from '../../Component/ProductCard';
+    import { useAuth } from '../../context/AuthContext';
+    import { useNavigate } from 'react-router-dom';
+    import { toast } from 'react-toastify';
+    import emptyWishList from '../../assets/empty_wishlist2.png'
 
-const Wish_List = () => {
-  const { wishlist } = useWishlist();
-   const { authUser } = useAuth();
-  const navigate = useNavigate();
-  const hasWarned = useRef(false);
+    const Wish_List = () => {
+      const { wishlist } = useWishlist();
+      const { authUser, loading } = useAuth();
+      const navigate = useNavigate();
+      const hasWarned = useRef(false);
 
-  useEffect(() => {
-    if (!authUser) {
-      navigate('/login'); // Redirect to login if not authenticated
-      if (!hasWarned.current) {
-        toast.warn("Please log in to view your wishlist",{        
-          style: {
-            background: "#199960", // dark background
-            color: "#fff",          // white text 
-          },  
-        });
-        hasWarned.current = true;
-      }
-    }
-  }, [authUser, navigate]);
+      useEffect(() => {
+        if (!loading &&  !authUser) {
+          navigate('/login'); // Redirect to login if not authenticated
+          if (!hasWarned.current) {
+            toast.warn("Please log in to view your wishlist",{        
+              style: { background: "#199960", color: "#fff"},  
+            });
+            hasWarned.current = true;
+          }
+        }
+      }, [authUser, loading, navigate]);
 
-  if (!authUser) {
-    return null; // Prevent rendering before redirect
-  }
 
-  return (
-    <div className='p-3'>
-      <h2 className='text-2xl font-bold mb-4'>Your Wishlist</h2>
-      {wishlist.length === 0 ? (
-        <p>No items in wishlist.</p>
-      ) : (
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
-          {wishlist.map((item) => (
-            <ProductCard
-              key={item.productId}
-              product={{
-                _id: item.productId,
-                name: item.productName,
-                image: [item.productImage],
-                price: item.offerPrice,
-                description: item.description?.split(','),
-              }}
-              showWishlistIcon={true}
-            />
-          ))}
+      return (
+        <div className='p-3 mt-[110px]'>
+          <h2 className='text-[10.5rem] font-bold mb-4 text-center'>Your Wishlist</h2>
+          {wishlist.length === 0 ? (
+            <div className="flex flex-col items-center justify-center min-h-screen text-center">
+              <img src={emptyWishList} alt="Empty Wishlist" className="w-48 h-48 object-contain mb-4"/>
+              <p className="text-lg text-gray-600">No items are present in your wishlist. Start Adding Your Favorite Products!</p>
+              <button onClick={() => navigate('/categories')} className="bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-3 rounded-md transition w-48">
+                Browse Products
+              </button>
+            </div>
+          ) : (
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
+              {wishlist.map((item) => (
+                <ProductCard
+                  key={item.productId}
+                  product={{
+                    _id: item.productId,
+                    name: item.productName,
+                    image: [item.productImage],
+                    price: item.offerPrice,
+                    description: item.description?.split(','),
+                  }}
+                  showWishlistIcon={true}
+                />
+              ))}
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  );
-};
+      );
+    };
 
-export default Wish_List;
+    export default Wish_List;
